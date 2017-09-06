f31ba63464964c41202590fa9e11ebb10f262b57
Ross Saur <ross@phin.co>
2017-09-06 11:45:19 -0700

    Initial Project
    Does not yet support async, needs refactored, and needs CI and code coverage.

---
+++ index.js
@@ -0,0 +1,119 @@
+( function(){
+	'use strict';
+
+	const httpMethods = ['del', 'get', 'head', 'opts', 'post', 'put', 'patch'];
+
+	function _supportPromises( restifyServer, options ){
+		if( !restifyServer ) throw new Error("Can't help you if you don't give me a server.");
+		if (!options) options = {};
+		const logger = options.logger || false;
+		const errorTransformer = options.customErrorHandler || false;
+
+		function _wrapRouteFunction( lastFunc ){
+			return function _wrappedRouteHandler( req, res, next ){
+
+				let nextCalled = false;
+				const newNext = function(){
+					if( nextCalled === true ) return;
+					nextCalled = true;
+					next.apply( null, arguments );
+				};
+
+				let routeInvocation = null;
+
+				try{
+					routeInvocation = lastFunc( req, res, newNext );
+				}catch( error ){
+					if( logger ) logger.error( "HANDLED ERROR: ", error );
+					let restError = !errorTransformer ? error  : errorTransformer.transform( error );
+					newNext( restError );
+					return;
+				}
+
+				if( _isPromise( routeInvocation ) ){
+					routeInvocation
+						.then( body => {
+							if( _shouldSendResponse( res ) ){
+								const statusCode = _extractStatusCodeFromBody( body );
+								res.status( statusCode );
+								res.send( body );
+							}
+							newNext();
+						})
+						.catch( error => {
+							if( logger ) logger.error( "HANDLED ERROR: ", error );
+							let restError = !errorTransformer ? error  : errorTransformer.transform( error );
+							newNext( restError );
+						});
+				}
+				else if( routeInvocation ){
+					// Route returned an object - let's send it
+					if( _shouldSendResponse( res ) ){
+						var statusCode = _extractStatusCodeFromBody( routeInvocation );
+						res.status( statusCode );
+						res.send( routeInvocation );
+					}
+					newNext();
+				}
+			};
+		}
+
+
+		httpMethods.forEach( method => {
+			const previous = restifyServer[method];
+			restifyServer[method] = function(){
+				const args = [].slice.call( arguments );
+				const handler = args[args.length - 1];
+
+				if( _isFunction( handler ) ){
+					const wrappedFunc = _wrapRouteFunction( handler );
+					args.splice( args.length - 1, 1, wrappedFunc );
+				}
+
+				previous.apply( restifyServer, args );
+			};
+		});
+	}
+
+	/**
+	 * NOTE: Modifies body
+	 *
+	 * Allows routes to change response status code by providing a statusCode property
+	 *
+	 */
+	function _extractStatusCodeFromBody( body ){
+		let statusCode = 200;
+
+		if( bodyHasStatusCode( body ) ){
+			// Allow resolved responses to change status code
+			statusCode = body.statusCode;
+			delete body.statusCode;
+		}
+		return statusCode;
+	}
+
+	function bodyHasStatusCode( body ){
+		if ( !body || typeof body !== 'object') return false;
+		return !!body.statusCode;
+	}
+
+	function _shouldSendResponse( res ){
+		return !res.finished && !res.headersSent;
+	}
+
+	function _isFunction( candidate ){
+		return !!candidate && typeof candidate === 'function';
+	}
+
+	function _isPromise( candidate ){
+		return !!candidate && (typeof candidate === 'object' || typeof candidate === 'function') && typeof candidate.then === 'function';
+	}
+
+	function _isAsync( candidate ){
+		return !!candidate && candidate[Symbol.toStringTag] === 'AsyncFunction';
+	}
+
+	exports.supportPromises = _supportPromises;
+
+
+})();
---
+++ tests/integrationTest.js
@@ -0,0 +1,252 @@
+(function() {
+	'use strict';
+	const describe = require('mocha').describe;
+	const assert = require('chai').assert;
+	const restify = require('restify');
+	const request = require('request-promise');
+	const promissor = require('../index');
+
+	describe('restify-await-promise', ()=>{
+
+		const host = "127.0.0.1";
+		const port = 5099;
+
+		let serverInstance = false;
+
+		beforeEach( done =>{
+			serverInstance = restify.createServer({name: 'testServer'});
+			promissor.supportPromises( serverInstance );
+			serverInstance.listen( port, host );
+			done();
+		});
+
+		afterEach( done =>{
+			try{
+				serverInstance.close( ()=>{
+					done();
+				});
+			}catch( ex ){
+				done( ex );
+			}
+		});
+
+		describe('general functions', ()=>{
+			it('should error when not given a server', ()=>{
+				assert.throws( () =>{
+					promissor.supportPromises( void 0 );
+				}, `Can't help you if you don't give me a server.`);
+			});
+		});
+
+		describe('Default Handler', ()=>{
+			it('should return the response when an object is returned', ()=>{
+				function promiseFunction( req, res, next ){
+					return  { success: true };
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 200 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isTrue( body.success );
+				});
+			});
+
+			it('should return the response when an object is returned', ()=>{
+				function promiseFunction( req, res, next ){
+					return  { success: true };
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 200 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isTrue( body.success );
+				});
+			});
+
+			it('should return the response when an object is returned with a status code', ()=>{
+				function promiseFunction( req, res, next ){
+					return  { success: true, statusCode: 205 };
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 205 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isTrue( body.success );
+					assert.isUndefined( body.statusCode );
+				});
+			});
+
+			it('should not blow up when the response is already set', ()=>{
+				function promiseFunction( req, res, next ){
+					res.status( 205 );
+					res.send( { success: false } );
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 205 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isFalse( body.success );
+				});
+			});
+
+		});
+
+		describe('Promise Handler', ()=>{
+
+			it('should return the response when the promise resolves', ()=>{
+				function promiseFunction( req, res, next ){
+					return Promise.resolve( { success: true} );
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+						method: 'GET',
+						uri: `http://${host}:${port}/path`,
+						resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 200 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isTrue( body.success );
+				});
+			});
+
+			it('should return the response when the promise resolves with nothing', ()=>{
+				function promiseFunction( req, res, next ){
+					return Promise.resolve();
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 200 );
+					assert.equal( '', result.body );
+				});
+			});
+
+			it('should not blow up if the response has already been set', ()=>{
+				function promiseFunction( req, res, next ){
+					res.send( { success: false} );
+
+					return Promise.resolve( { success: true} );
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 200 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isFalse( body.success );
+				});
+			});
+
+			it('should not blow up if the response has already been set and next is called', ()=>{
+				function promiseFunction( req, res, next ){
+					res.send( { success: false} );
+					next();
+					return Promise.resolve();
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 200 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isFalse( body.success );
+				});
+			});
+
+			it('should not blow up if the response has already been set and next has been called', ()=>{
+				function promiseFunction( req, res, next ){
+					res.send( { success: false} );
+					next();
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				}).then( result =>{
+					assert.equal( result.statusCode, 200 );
+					assert.ok( result.body );
+					let body = JSON.parse( result.body );
+					assert.isFalse( body.success );
+				});
+			});
+
+			it('should handle the promise rejecting when the reject has a status code', ()=>{
+				function promiseFunction( req, res, next ){
+					let errToThrow = new Error('See ya!');
+					errToThrow.statusCode = 415;
+					return Promise.reject( errToThrow );
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				})
+				.then( result =>{
+					assert.fail( 'Should reject here');
+				}).catch( ex =>{
+					assert.equal( ex.statusCode, 415 );
+					assert.equal( JSON.parse(ex.error).message, 'See ya!');
+				});
+			});
+
+			it('should handle the promise rejecting when the reject does not have a status code', ()=>{
+				function promiseFunction( req, res, next ){
+					let errToThrow = new Error('See ya!');
+					return Promise.reject( errToThrow );
+				}
+				serverInstance.get( '/path', promiseFunction );
+				return request({
+					method: 'GET',
+					uri: `http://${host}:${port}/path`,
+					resolveWithFullResponse: true
+				})
+				.then( result =>{
+					assert.fail( 'Should reject here');
+				}).catch( ex =>{
+					assert.equal( ex.statusCode, 500 );
+					assert.equal( JSON.parse(ex.error).message, 'See ya!');
+				});
+			});
+		});
+
+		describe('Async Handler', ()=>{
+
+
+		});
+
+	});
+
+
+})();
